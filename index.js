983799a4c0187c521d8219001e3b677a2fbb8e7e
andy-aguilar <aaguil3@gmail.com>
2020-09-23 14:18:38 -0500

    passed second test

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 // Code your solution here!
 
+function printString(myString) {
+    if (myString.length > 1){
+        console.log(myString[0])
+        printString(myString.substring(1, myString.length))
+    }
+    else {
+        console.log(myString)
+    }
+}
+
+
+function reverseString(myString) {
+    if (myString.length > 1){
+        return reverseString(myString.substring(1, myString.length)) + myString[0]
+    }
+    else{
+        return myString
+    }
+}
+
+function isPalindrome(myString){
+    if (myString.length > 1){
+        if(myString[0] === myString[myString.length -1]){
+            return isPalindrome(myString.substring(1, myString.length - 1))
+        }
+        else{
+            return false
+        }
+    }
+    else{
+        return true
+    }
+}
+
+
+
+
